a332ee8c5267d862870db2095aa5c2113a416461
alexsantiago2023 <alexsantiago2003.2@gmail.com>
2025-09-25 12:34:15 -0600

    Finished Activity prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 100);
+
+        int guess = 0;
+
+        while (guess != magic)
+        {
+            Console.Write("What's your guess? ");
+            string input2 = Console.ReadLine();
+            guess = int.Parse(input2);
+            if (guess > magic)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magic)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess == magic)
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+        }
     }
 }
