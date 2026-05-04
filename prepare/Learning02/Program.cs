5bcc9b0223cc3ffd7e95dfdda207e597ca5b75a2
alexsantiago2023 <alexsantiago2003.2@gmail.com>
2026-05-04 15:00:24 -0600

    started Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,7 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Console.WriteLine("Hello Learning02 World!");
-        
+        Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._jobTitle = "SOC Analyst";
+        job2.DisplayJobDetails();
+
     }
 }
