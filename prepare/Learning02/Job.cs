c54fc6f53a9b8b3e6f1b221878a9a90cd2bfad1f
alexsantiago2023 <alexsantiago2003.2@gmail.com>
2026-05-04 17:08:17 -0600

    Finished Learning02

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -5,8 +5,8 @@ public class Job
     public int _startYear;
     public int _endYear;
 
-    public void DisplayJobDetails()
+    public void Display()
     {
-        Console.WriteLine(_jobTitle);
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
 }
