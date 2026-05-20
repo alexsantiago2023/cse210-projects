578253fb133fe4ccbdffb668dd257b83714ffafd
alexsantiago2023 <alexsantiago2003.2@gmail.com>
2026-05-20 14:35:21 -0600

    added angle sandbox

---
+++ sandbox/Angle/Angle.cs
@@ -0,0 +1,44 @@
+using System.ComponentModel.Design;
+
+class Angle
+{
+    private double _radians;
+
+    public Angle()
+    {
+        _radians = 0;
+    }
+
+    public Angle(double radians)
+    {
+        _radians = radians;
+    }
+
+    public Angle(float degrees)
+    {
+        _radians = ToRadians(degrees);
+    }
+
+    public double GetRadians()
+    {
+        return _radians;
+    }
+
+    public void SetRadians(double radians)
+    {
+        if (radians < 0)
+        {
+            Console.WriteLine("Invalid data");
+            _radians = 0;
+        } 
+        else
+        {
+            _radians = radians;
+        }
+    }
+
+    private double ToRadians(double degrees)
+    {
+        return degrees / 180 * Math.PI;
+    }
+}
