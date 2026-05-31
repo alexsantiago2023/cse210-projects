759e5cf94860299073b5e1bad7eef7a1f23e1c63
alexsantiago2023 <alexsantiago2003.2@gmail.com>
2026-05-30 22:00:45 -0600

    finished Develop03

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -8,6 +8,8 @@ class Scripture
     private int _endVerse;
     private string _content;
     private string _reference;
+    List<Word> _words = new List<Word>();
+    private Random random = new Random();
 
     public Scripture(string book, int chapter, int startVerse, string content)
     {
@@ -18,6 +20,13 @@ class Scripture
 
         Reference tempRef = new Reference(_book, _chapter, _startVerse);
         _reference = tempRef.GetScriptureReference();
+
+        foreach (string word in content.Split(' '))
+        {
+            Word tempWord = new Word(word);
+            _words.Add(tempWord);
+            //tempWord.Hide();
+        }
     }
 
     public Scripture(string book, int chapter, int startVerse, int endVerse, string content)
@@ -30,22 +39,58 @@ class Scripture
 
         Reference tempRef = new Reference(_book, _chapter, _startVerse, _endVerse);
         _reference = tempRef.GetScriptureReference();
-    }
 
-    public List<Word> ConvertToWords(string text)
-    {
-        List<Word> words = new List<Word>();
-        foreach (string word in text.Split(' '))
+        foreach (string word in content.Split(' '))
         {
             Word tempWord = new Word(word);
-            words.Add(tempWord);
-            tempWord.Hide();
+            _words.Add(tempWord);
+            //tempWord.Hide();
         }
-        return words;
     }
 
     public void Display()
     {
-        Console.WriteLine(_reference);
+        Console.Clear();
+        Console.WriteLine("\n" + _reference);
+        foreach (Word word in _words)
+        {
+            Console.Write(word.GetWordString() + " ");
+        }
+        Console.WriteLine($"\n\nPress enter to continue or type 'quit' to finish:");
+    }
+
+    public bool HiddenMax()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void HideWords(int amt)
+    {
+        //I got this _hiddenCount syntax from AI but I didn't know how else to do it.
+        int _hiddenCount = _words.Count(w => !w.IsHidden());
+        if (_hiddenCount < amt)
+        {
+            amt = _hiddenCount;
+        }
+
+        for (int i = 0; i < amt; i++){
+
+            int index;
+            do
+            {
+                index = random.Next(0, _words.Count);   
+            } 
+            while (_words[index].IsHidden());
+
+            _words[index].Hide();
+        }
     }
 }
