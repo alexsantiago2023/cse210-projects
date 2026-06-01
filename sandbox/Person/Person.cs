0524a35a0aea94e3afa8e71bc4e756ad5ab58168
alexsantiago2023 <alexsantiago2003.2@gmail.com>
2026-06-01 14:59:25 -0600

    Inheritance program

---
+++ sandbox/Person/Person.cs
@@ -0,0 +1,19 @@
+class Person
+{
+    private string _firstName;
+    private string _lastName;
+    private int _age;
+    private int _weight;
+    public Person(string firstName, string lastName, int age, int weight)
+    {
+        _firstName = firstName;
+        _lastName = lastName;
+        _age = age;
+        _weight = weight;
+    }
+
+    public string GetPersonInfo()
+    {
+        return $"Name: {_firstName} {_lastName}, Age: {_age}, Weight: {_weight}";
+    }
+}
---
+++ sandbox/Person/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Security;
+using System.Reflection.Metadata;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello world");
+
+        Person me = new Person("Alex", "Santiago", 22, 180);
+        Console.WriteLine(me.GetPersonInfo());
+
+        Police officer = new Police("gun", "Robert", "Hales", 45, 230);
+        Console.WriteLine(officer.GetPoliceManInfo());
+    }
+}
