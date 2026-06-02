06694f4f854731966f1225555be2a00a7d78bb5e
alexsantiago2023 <alexsantiago2003.2@gmail.com>
2026-06-01 22:54:12 -0600

    Finished Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        Console.WriteLine();
+
+        MathAssignment math1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment writing1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing1.GetSummary());
+        Console.WriteLine(writing1.GetWritingInformation());
+
     }
 }
