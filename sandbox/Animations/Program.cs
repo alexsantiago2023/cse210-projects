5ff979a1b2ab62cf3d6f242c2be2159b9b57845c
alexsantiago2023 <alexsantiago2003.2@gmail.com>
2026-06-10 14:24:36 -0600

    finsihed animation

--- sandbox/Animations/Program.cs
+++ sandbox/Animations/Program.cs
@@ -19,13 +19,30 @@ class Program
             Console.Write("\b");
         }*/
 
-        string animationString = "-\\|/";
-        int index = 0;
-        for (int i = 0; i < 30; i++)
+        // string animationString = "-\\|/";
+        // int index = 0;
+
+        DateTime now = DateTime.Now;
+        DateTime endTime = now.AddSeconds(15);
+        Console.WriteLine(now.ToString());
+
+        // while (DateTime.Now < endTime)
+        // {
+        //     Console.Write(animationString[index++ % animationString.Length]);
+        //     Thread.Sleep(sleepTime);
+        //     Console.Write("\b");
+        // }
+        // Console.WriteLine(endTime.ToString());
+
+        Console.CursorVisible = true;
+
+        int count = 15;
+        while (DateTime.Now < endTime)
         {
-            Console.Write(animationString[index++ % animationString.Length]);
-            Thread.Sleep(sleepTime);
-            Console.Write("\b");
+            Console.Write($"{count--, 2}");
+            Thread.Sleep(1000);
+            Console.Write("\b\b");
         }
     }
+
 }
