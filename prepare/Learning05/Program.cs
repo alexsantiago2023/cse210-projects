b921fdd075aad3d7e561798ec2357062be345c86
alexsantiago2023 <alexsantiago2003.2@gmail.com>
2026-06-15 14:12:08 -0600

    Finished Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> _shapes = new List<Shape>();
+
+        _shapes.Add(new Square("red", 5));
+        _shapes.Add(new Rectangle("orange", 3, 12));
+        _shapes.Add(new Circle("blue", 4));
+
+        foreach (Shape s in _shapes)
+        {
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine(s.GetArea());
+        }
     }
 }
