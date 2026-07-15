e1ea174777cc4228d3f9b331c5adad5e1be3f946
alexsantiago2023 <alexsantiago2003.2@gmail.com>
2026-07-15 13:56:51 -0600

    restarting final

--- final/FinalProject/Mage.cs
+++
@@ -1,8 +0,0 @@
-public class Mage : Player
-{
-    public Mage(string name)
-    {
-        _name = name;
-    }
-
-} 
--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,10 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Menu menu = new Menu();
-        menu.DisplayMenu();
-
-        // Knight sir_eladrian = new Knight("Sir Eladrian");
-        // sir_eladrian.Attack();
+        
     }
 }
